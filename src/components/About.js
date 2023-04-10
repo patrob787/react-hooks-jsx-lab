7832ac68a2b51142454262c4573a010376216530
Patrick Roberts <patrob22@gmail.com>
2023-04-10 14:07:51 -0400

    lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>When I was a kid, I wanted to be a Circus performer.  This isn't that.  But it pays the bills so....I guess it's fine.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
